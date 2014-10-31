1117ad42d43271c5605cae9681e98e0a703cf466
User <User@WIN-G0BIR1262KR>
2014-10-31 09:51:57 +0800

    asd

---
+++ src/jenkinsTest/StringAdd.java
@@ -0,0 +1,19 @@
+/* 
+ * @author
+ */
+
+package jenkinsTest;
+
+public class StringAdd {
+
+	
+	public static void main(String[] args){
+		StringAdd test = new StringAdd();
+		String add = test.addTogether("Hello", "World");
+		System.out.println(add);
+	}
+	public String addTogether(String one, String two){
+		return one+two;
+	}
+	
+}
---
+++ src/jenkinsTest/TestTest.java
@@ -0,0 +1,16 @@
+package jenkinsTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void testAddTogether() {
+		StringAdd test = new StringAdd();
+		String result = test.addTogether("one", "two");
+		assertEquals("onetwo", result);
+	}
+
+}
