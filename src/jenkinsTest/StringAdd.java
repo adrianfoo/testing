56d12c572a262fd1ecdd5773b8ae71158c06fe43
User <User@WIN-G0BIR1262KR.com>
2014-11-03 15:32:06 +0800

    asdsadsadsadsadsadsadsadads

---
+++ src/jenkinsTest/StringAdd.java
@@ -0,0 +1,16 @@
+/*
+ * @author Adrian
+ */
+
+package jenkinsTest;
+
+public class StringAdd {
+	public static void main(String[] args){
+		StringAdd add = new StringAdd();
+		System.out.println(add.addTogether("one", "two"));
+	}
+	public String addTogether(String one, String two){
+		return one+two;
+	}
+
+}
